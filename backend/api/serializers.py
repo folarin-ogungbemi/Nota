d51b5192281fbfe03460d78b826270f8832cb63b
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-06-16 22:36:23 +0200

    Create react app frontend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,4 +5,6 @@ from .models import Notes
 class NoteSerializer(ModelSerializer):
     class Meta:
         model = Notes
+        # we can also manually specify the fields
+        # fields = ['body', 'content']
         fields = '__all__'
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,19 @@ from django.http import JsonResponse
 from api.models import Notes
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+# import notes
 from .models import Notes
 from .serializers import NoteSerializer
 
 
 @api_view(['GET'])
 def getNotes(request):
+    # get the data from the DB
     notes = Notes.objects.all()
+    # Next we have to serialize the data 
+    # from python object to a json format for trasfer purposes
+    # 'many' specifies that we are serializing many objects
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
@@ -17,5 +23,6 @@ def getNotes(request):
 @api_view(['GET'])
 def getNote(request, pk):
     note = Notes.objects.get(id=pk)
+    # 'many' specifies that we are serializing one object
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
