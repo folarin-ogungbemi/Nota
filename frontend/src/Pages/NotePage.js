83a786f038b70f83c4cdaadb644deaa484281de6
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-06-18 20:56:58 +0200

    Add styles

--- frontend/src/Pages/NotePage.js
+++ frontend/src/Pages/NotePage.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const NotePage = () => {
-  return (
-    <div>NotePage</div>
+const NotePage = ({match}) => {
+
+  // the ".id" is the name we passed in our NotePage Route path
+  let noteId = match.params.id
+
+  const [note, setNote] = useState(null)
+
+  useEffect(() =>{
+    let getNote = async () =>{
+    const response = await fetch(`/api/notes/${noteId}/`)
+    const data = await response.json
+    setNote(data)
+    }
+
+    getNote()
+  }, [noteId])
+  
+  return ( 
+    <div className='note'>
+      {/* the question mark in the p tag makes sure
+       that we do not get error if note.body is not found */}
+      <p>{note?.body}</p>
+    </div>
   )
 }
 
--- frontend/src/Pages/NotesListPage.js
+++ frontend/src/Pages/NotesListPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ListItem from '../components/ListItem'
 
 const NotesListPage = () => {
 
@@ -11,18 +12,22 @@ const NotesListPage = () => {
     let getNotes = async () => {
         let response = await fetch('/api/notes/')
         let data = await response.json()
-        console.log("DATA", data)
         setNotes(data)
     }
 
   return (
-    <div>
-        <div>
-            {notes.map((note, index) => (
-                <h3 key={index}>{note.body}</h3>
-            ))}
+    <div className="notes">
+            <div className="notes-header">
+                <h2 className="notes-title">&#9782; Notes</h2>
+                <p className="notes-count">{notes.length}</p>
+            </div>
+
+            <div className="notes-list">
+                {notes.map((note, index) => (
+                    <ListItem key={index} note={note} />
+                ))}
+            </div>
         </div>
-    </div>
   )
 }
 
