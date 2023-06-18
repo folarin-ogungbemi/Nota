498c76944b5a933ebb1cd85295c7370942650c56
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-06-18 18:28:30 +0200

    import react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import Header from './components/Header'
-import NotesListPage from './components/NotesListPage'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+ } from 'react-router-dom';
 
-const App = () => {
+import React from "react"
+import Header from './components/Header'
+import NotesListPage from './Pages/NotesListPage'
+import NotePage from './Pages/NotePage'
+
+function App() {
   return (
-    <div>
-      <Header/>
-      <NotesListPage/>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<NotesListPage/>} />
+          <Route path="/note/:id" element={<NotePage/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
